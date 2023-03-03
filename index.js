48202445943d0127dd3cdc203c8a8ae437bb1a92
Zachary Rojas <ztr2194@gmail.com>
2023-03-03 17:04:35 -0600

    looping lab complete

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+const messages = [];
+
+function writeCards(names, event) {
+    const card = [...messages]
+    for (let n = 0; n < names.length; n++) {
+    messages.push(`Thank you, ${names[n]}, for the wonderful ${event} gift!`);
+}
+    return card;
+}
+writeCards(["Guadalupe", "Ollie", "Aki"], "surprise");
+
+function countDown() {
+    let n = 10;
+    while (n >= 0) {
+        console.log(n);
+    n--;
+    }
+}
